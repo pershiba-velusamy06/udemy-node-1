7be0a5b579eb0227337ccd608c8b0a55dbfbd731
pershiba-velusamy06 <pershiba@elred.io>
2024-01-10 22:04:56 +0530

    sending htmlmpage as response from server

--- src/index.js
+++ src/index.js
@@ -1,24 +1,36 @@
-import express from 'express';
+// import express from 'express';
 
+
+// const app = express();
+
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use('/', (req,res,next)=>{
+//   console.log('I m the first')
+//   //res.send('I m the first')
+//   next()
+// });
+// app.use('/user',(req,res)=>{
+//   console.log('I m the second')
+//   res.send('I m the second')
+
+// })
+
+
+
+
+const path = require('path');
+
+const express = require('express');
+
+const mainRoutes = require('./routes/index');
 
 const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use('/', (req,res,next)=>{
-  console.log('I m the first')
-  //res.send('I m the first')
-  next()
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(mainRoutes);
+
+app.listen(3000,()=>{
+  console.log('node server is running in 3000')
 });
-app.use('/user',(req,res)=>{
-  console.log('I m the second')
-  res.send('I m the second')
-
-})
-
-app.listen(3000, () => {
-    console.log("Node server is running in port 3000");
-})
-app.timeout = 60000;
-
-export default app
---
+++ src/public/main.js
@@ -0,0 +1,1 @@
+alert('You are seeing user lists!');
